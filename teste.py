f06c0c93fb84a50b3e145b63d9e99efe86d3e4d5
guilhermegdf <36552622+guilhermegdf@users.noreply.github.com>
2018-03-22 12:45:54 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -2,7 +2,7 @@
 import funcoes
 
 x="Renato"
-listar = funcoes.buscar(x)
+#listar = funcoes.buscar(x)
 
-assert listar == "Renato|123"
-print(listar)
+assert funcoes.buscar(x) == "Renato|123"
+#print(listar)
